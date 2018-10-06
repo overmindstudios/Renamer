02746bc49c8c920bdbb6a3a9059f63731dc13be4
tobi <tobi@overmind-studios.de>
2018-10-06 19:27:56 +0200

    initial commit

---
+++ Ui_renamer.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'renamer.ui',
+# licensing of 'renamer.ui' applies.
+#
+# Created: Sat Oct  6 17:15:38 2018
+#      by: pyside2-uic  running on PySide2 5.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(607, 347)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 601, 51))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.comboBox = QtWidgets.QComboBox(self.horizontalLayoutWidget)
+        self.comboBox.setMinimumSize(QtCore.QSize(350, 0))
+        self.comboBox.setObjectName("comboBox")
+        self.horizontalLayout.addWidget(self.comboBox)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
+        self.label.setText(QtWidgets.QApplication.translate("MainWindow", "Project config:", None, -1))
+
---
+++ renamer.py
@@ -0,0 +1,69 @@
+import Ui_renamer
+import setstyle
+import sys
+import os
+import fnmatch
+import yaml
+
+if getattr(sys, 'frozen', False):  # Do a check if running from frozen application or .py script
+    from PySide2 import QtWidgets, QtCore, QtGui
+else:
+    try:
+        from PyQt5 import QtWidgets, QtCore, QtGui
+    except:
+        print("PyQt5 not found, trying PySide2...")
+        from Qt import QtWidgets, QtCore, QtGui
+
+appversion = '0.1'
+available_configs = {}
+
+class MainWindow(QtWidgets.QMainWindow, Ui_renamer.Ui_MainWindow):
+    def __init__(self):
+        '''Construct MainWindow'''
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle("Overmind Studios Renamer " + appversion)
+        self.statusbar.setSizeGripEnabled(False)
+        self.setFixedSize(self.size())
+        self.show()
+        self.comboBox.currentIndexChanged.connect(self.on_combobox_changed)
+        self.parseConfigs()
+
+    def parseConfigs(self):
+        '''parse config files and populate fields'''
+        global available_configs
+        config_index = 0
+        configlist = os.listdir('configs')
+        for config in configlist:
+            if fnmatch.fnmatch(config, '*.yml'):
+                with open(os.path.join('configs', config), 'r') as stream:
+                    try:
+                        data = yaml.load(stream)
+                        available_configs[config_index] = config
+                        config_index += 1
+                        self.comboBox.addItem(str('{0:03d}'.format(config_index)) + " - " + str(data.get('date')) + " - " + data.get('client') + " - " + data.get('project') + " - " + data.get('version'))
+                        print(available_configs)
+                    except yaml.YAMLError as exc:
+                        print(exc)
+    
+    def on_combobox_changed(self, value):
+        '''update configuration according to user selection'''
+        global available_configs
+        with open(os.path.join('configs', available_configs[value]), 'r') as stream:
+                    try:
+                        data = yaml.load(stream)
+                        self.statusbar.showMessage("Naming scheme: " + data.get('naming_scheme'))
+                    except yaml.YAMLError as exc:
+                        print(exc)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyle('Fusion')
+    app.setPalette(setstyle.setPalette())
+    window = MainWindow()
+
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
